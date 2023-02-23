dc29b76571a2306413cf290dfbcf201c075c4a28
sniffenz <96217756+sniffenz@users.noreply.github.com>
2023-02-23 11:08:42 -1000

    Update index.js
    Added humidity and wind speed collection and wrote to the HTML page, and reworked the code to accept the city name rather than the zipcode from the HTML page.

--- index.js
+++ index.js
@@ -14,14 +14,14 @@ app.get("/", function(req, res) {
 //invoked after hitting go in the html form
 app.post("/", function(req, res) {
     
-    // takes in the zip from the html form, display in // console. Takes in as string, ex. for zip 02139
-        var zip = String(req.body.zipInput);
-        console.log(req.body.zipInput);
+    // takes in the city from the html form, display in // console. Takes in as string, ex. for kahului
+        var incity = String(req.body.cityInput);
+        console.log(req.body.cityInput);
     
     //build up the URL for the JSON query, API Key is // secret and needs to be obtained by signup 
         const units = "imperial";
-        const apiKey = "67f6b382921c1e89b39b20d4f9556f22";
-        const url = "https://api.openweathermap.org/data/2.5/weather?zip=" + zip +  "&units=" + units + "&APPID=" + apiKey;
+        const apiKey = "b6304e5650dc46d1c2e0c54806e5ac4f";
+        const url = "https://api.openweathermap.org/data/2.5/weather?q=" + incity+"&units=" + units + "&APPID=" + apiKey;
     
     // this gets the data from Open WeatherPI
     https.get(url, function(response){
@@ -31,15 +31,19 @@ app.post("/", function(req, res) {
         response.on("data", function(data){
             const weatherData = JSON.parse(data);
             const temp = weatherData.main.temp;
-            const city = weatherData.name;
+            const city = incity;
             const weatherDescription = weatherData.weather[0].description;
             const icon = weatherData.weather[0].icon;
             const imageURL = "http://openweathermap.org/img/wn/" + icon + "@2x.png";
+            const humidity = weatherData.main.humidity;
+            const windspeed = weatherData.wind.speed;
             
             // displays the output of the results
             res.write("<h1> The weather is " + weatherDescription + "<h1>");
-            res.write("<h2>The Temperature in " + city + " " + zip + " is " + temp + " Degrees Fahrenheit<h2>");
+            res.write("<h2>The Temperature in " + city + " is " + temp + " Degrees Fahrenheit<h2>");
             res.write("<img src=" + imageURL +">");
+            res.write("<h2> The humidity in " + city + " is "+ humidity + "% .<h2>");
+            res.write("<h2> The wind speed in " + city + " is "+ windspeed + " mph(miles per hour). ");
             res.send();
         });
     });
